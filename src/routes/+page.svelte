eb2415907e8f717cfae546d95b8f690bea10b0e4
Cai Bear <git@caibear.com>
2025-03-12 22:03:19 -0700

    Add category filter sidebar.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <script>
     import { Github } from '@lucide/svelte';
     import games from '$lib/games.json';
+    import categories from '$lib/categories.json';
+
+    let selectedCategories = [];
 </script>
 
 <header>
@@ -9,22 +12,41 @@
     <a href="https://github.com/caibear/realmultiplayer" target="_blank"><Github/></a>
 </header>
 <br/>
-<main>
-  {#each games as game}
-    {@const yourGame = game.name == "yourgame"}
-    <div class="game" class:yourgame={yourGame}>
-        <a href={`https://${game.domain}`} target="_blank">
-            {#if yourGame}
-                <div class="fakeimg"></div>
-                <h3 class="yourgame" style="text-align:center">Your game here!</h3>
-            {:else}
-                <img src={`/${game.name}_teaser.webp`} alt={game.name}/>
-                <h3 class="domain">{game.domain}</h3>
-            {/if}
-        </a>
+
+<div style="display: flex; flex-direction: row;">
+    <div id="sidebar">
+        {#each categories as category}
+            {@const selected = selectedCategories.includes(category)}
+            <!-- svelte-ignore a11y_click_events_have_key_events -->
+            <!-- svelte-ignore a11y_no_static_element_interactions -->
+            <div class="category" class:selected={selected} on:click={() => {
+                if (selected) {
+                    selectedCategories = selectedCategories.filter(c => c != category);
+                } else {
+                    selectedCategories = [...selectedCategories, category];
+                }
+            }}>{category}</div>
+        {/each}
     </div>
-  {/each}
-</main>
+
+    <main>
+        {#each games.filter(game => selectedCategories.length == 0 || selectedCategories.every(c => (game.categories || []).includes(c))) as game}
+          {@const yourGame = game.name == "yourgame"}
+          <div class="game" class:yourgame={yourGame}>
+              <a href={`https://${game.domain}`} target="_blank">
+                  {#if yourGame}
+                      <div class="fakeimg"></div>
+                      <h3 class="yourgame" style="text-align:center">Your game here!</h3>
+                  {:else}
+                      <img src={`/${game.name}_teaser.webp`} alt={game.name}/>
+                      <h3 class="domain">{game.domain}</h3>
+                  {/if}
+              </a>
+          </div>
+        {/each}
+    </main>
+</div>
+
 
 <style>
     html, body {
@@ -53,6 +75,19 @@
         flex-wrap: wrap;
     }
 
+    #sidebar {
+        margin-right: 0.5rem;
+        border-radius: 1rem;
+        width: 22rem;
+        height: 32rem;
+        display: flex;
+        flex-direction: column;
+        background-color: #28283c;
+        z-index: 5;
+        padding: 0.5rem;
+        gap: 0.5rem;
+    }
+
     h1 {
         margin: 0;
         letter-spacing: .2rem;
@@ -75,12 +110,27 @@
 
     div.game {
         position: relative;
-        width: 20rem;
-        height: 20rem;
+        width: 18rem;
+        height: 18rem;
         border-radius: 1rem;
         overflow: hidden;
         border: 2px solid transparent;
         transition: border-color 0.25s;
+    }
+
+    div.category {
+        padding: 0.5rem;
+        background-color: black;
+        border-radius: 0.5rem;
+        border: 1px solid transparent;
+    }
+
+    div.category:hover {
+        border-color: white;
+    }
+
+    div.category.selected {
+        background-color: gray;
     }
 
     div.yourgame {
