a41d347e61a6400b6a2d0940610f7aac4a159419
Finn Bear <finnbearlabs@gmail.com>
2025-03-25 12:32:26 -0700

    Add Ships 3D and Tanks 3D.

--- src/lib/Games.svelte
+++ src/lib/Games.svelte
@@ -5,7 +5,7 @@
 
     export let selectedCategories = [];
 
-    let seed = 1; // Date.now() / (24 * 60 * 60 * 1000);
+    let seed = Math.floor(Date.now() / (24 * 60 * 60 * 1000));
 
     if (false) {
         setInterval(() => {
